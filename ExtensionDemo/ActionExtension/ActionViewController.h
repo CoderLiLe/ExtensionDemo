40f6e8e2984990ffba6072bb0179451bc8e4c91e
李乐 <LiLeiOS@163.com>
2017-03-29 16:28:05 +0800

    添加语音文字转语音

--- ExtensionDemo/ActionExtension/ActionViewController.h
+++ ExtensionDemo/ActionExtension/ActionViewController.h
@@ -5,6 +5,7 @@
 //  Created by LiLe on 2017/3/27.
 //  Copyright © 2017年 LiLe. All rights reserved.
 //
+// 参考文章：http://mobile.51cto.com/iphone-464809.htm
 
 #import <UIKit/UIKit.h>
 
--- ExtensionDemo/ActionExtension/ActionViewController.m
+++ ExtensionDemo/ActionExtension/ActionViewController.m
@@ -8,10 +8,12 @@
 
 #import "ActionViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ActionViewController ()
 
 @property(strong,nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -20,6 +22,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self SpeechSynthesizer];
+}
+
+- (void)setImage {
     // Get the item[s] we're handling from the extension context.
     
     // For example, look for an image and place it into an image view.
@@ -50,15 +56,45 @@
     }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)SpeechSynthesizer
+{
+    // 从扩展背景信息中获取我们打算处理的项目。
+    // 在我们的Action extension当中, 我们只需要一个输入项目（即文本），因此我们使用数组中的第一个项目。
+    NSExtensionItem *item = self.extensionContext.inputItems[0];
+    NSItemProvider *itemProvider = item.attachments[0];
+    
+    if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypePlainText]) {
+        
+        // 这是一段纯文本！
+        __weak UITextView *textView = self.textView;
+        
+        [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypePlainText options:nil completionHandler:^(NSString *item, NSError *error) {
+            
+            if (item) {
+                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                    
+                    [textView setText:item];
+                    
+                    // 设置语音合成并加以启动
+                    AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc] init];
+                    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:textView.text];
+                    [utterance setRate:0.1];
+                    [synthesizer speakUtterance:utterance];
+                }];
+            }
+        }];
+    }
 }
 
 - (IBAction)done {
     // Return any edited content to the host app.
     // This template doesn't do anything, so we just echo the passed in items.
     [self.extensionContext completeRequestReturningItems:self.extensionContext.inputItems completionHandler:nil];
+}
+- (IBAction)actionButtonPressed:(UIBarButtonItem *)sender {
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[self.textView.text]
+                                                                             applicationActivities:nil];
+    [self presentViewController:activityVC animated:YES completion:nil];
 }
 
 @end
