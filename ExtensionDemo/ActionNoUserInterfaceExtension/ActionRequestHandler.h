71c5a98ac890d826c2a2468cd5c84fa9253df22e
李乐 <LiLeiOS@163.com>
2017-03-29 16:27:13 +0800

    创建ActionNoUserInterfaceExtension

---
+++ ExtensionDemo/ActionNoUserInterfaceExtension/ActionRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  ActionRequestHandler.h
+//  ActionNoUserInterfaceExtension
+//
+//  Created by LiLe on 2017/3/29.
+//  Copyright © 2017年 LiLe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionRequestHandler : NSObject <NSExtensionRequestHandling>
+
+@end
---
+++ ExtensionDemo/ActionNoUserInterfaceExtension/ActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActionViewController.h
+//  ActionNoUserInterfaceExtension
+//
+//  Created by LiLe on 2017/3/29.
+//  Copyright © 2017年 LiLe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionViewController : UIViewController
+
+@end
